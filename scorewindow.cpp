b2b50b43f99cfd729bf2b72522a6a258e8d53d0f
RaphDesaegher <raphael.desaegher@gmail.com>
2013-12-17 19:02:50 +0100

    Remerge

--- scorewindow.cpp
+++ scorewindow.cpp
@@ -21,19 +21,15 @@ ScoreWindow::ScoreWindow(QWidget *parent,int timeElapsed,QString track) :
     m_scoreManager(new ScoreManager(this))
 {
     ui->setupUi(this);
-<<<<<<< HEAD
     this->setStyleSheet("#ScoreWindow{background-image : url(./data/img/Fond.png);}");
     ui->pushButton->setStyleSheet("QPushButton{background-color:rgba(255,255,255,0);background-image:url(./data/img/CONTINUE.png);} QPushButton::hover{background-image:url(./data/img/CONTINUE-COULEUR.png);}");
     ui->label->setStyleSheet("QLabel#label{background-image:url(./data/img/FOND-SCORE.png);}");
-
-=======
 
     connect(m_scoreManager, SIGNAL(scoreReceived(QString,QVector<Score*>)), this, SLOT(loaded(QString,QVector<Score*>)));
     connect(m_scoreManager, SIGNAL(error(QString)), this, SLOT(error(QString)));
 
     m_scoreManager->getScores(m_track);
     /*
->>>>>>> 222aa4f23bbea0be87c9dad593648be073bb6622
     load();
     m_rank = ranked();
     if(m_rank<10)
